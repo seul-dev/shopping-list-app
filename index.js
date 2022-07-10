fef84f24a159c208b41cf5b4a12eac43c00cfeda
seul-dev <seul.dev@gmail.com>
2022-07-11 00:06:26 +0900

    localStorage 저장 기능 추가

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
-//입력창 내용 -> 리스트아이템 추가
-// 버튼 클릭시, enter키
-// delete 버튼 클릭시 -> 리스트 아이템 삭제
-// item local storage에 저장하기
-
 const items = document.querySelector('.items');
 const input = document.querySelector('.footer__input');
 const addBtn = document.querySelector('.footer__button');
 
-function createItem(newItem) {
+const Items = 'items';
+let shoppingItems = [];
+
+const savedItems = localStorage.getItem(Items);
+if (savedItems !== null) {
+  const parsedItems = JSON.parse(savedItems);
+  shoppingItems = parsedItems;
+  parsedItems.forEach(paintItem);
+}
+
+function saveShoppingItems() {
+  localStorage.setItem(Items, JSON.stringify(shoppingItems));
+}
+
+function deleteItem(itemRowId) {
+  shoppingItems = shoppingItems.filter(
+    (item) => item.id !== parseInt(itemRowId)
+  );
+  saveShoppingItems();
+}
+
+function paintItem(newItem) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'item__row');
+  itemRow.id = newItem.id;
 
   const item = document.createElement('div');
   item.setAttribute('class', 'item');
 
   const span = document.createElement('span');
   span.setAttribute('class', 'item__name');
-  span.innerText = newItem;
+  span.innerText = newItem.text;
 
   const deleteBtn = document.createElement('button');
   deleteBtn.setAttribute('class', 'item__delete');
   deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-  deleteBtn.addEventListener('click', () => items.removeChild(itemRow));
+  deleteBtn.addEventListener('click', () => {
+    items.removeChild(itemRow);
+    deleteItem(itemRow.id);
+  });
 
   const itemDividor = document.createElement('div');
   itemDividor.setAttribute('class', 'item__dividor');
@@ -30,7 +50,8 @@ function createItem(newItem) {
   item.appendChild(deleteBtn);
   itemRow.appendChild(item);
   itemRow.appendChild(itemDividor);
-  return itemRow;
+  items.appendChild(itemRow);
+  itemRow.scrollIntoView({ block: 'center' });
 }
 
 function onAdd() {
@@ -39,9 +60,10 @@ function onAdd() {
     input.focus();
     return;
   }
-  const item = createItem(newItem);
-  items.appendChild(item);
-  item.scrollIntoView({ block: 'center' });
+  const newItemObj = { id: Date.now(), text: newItem };
+  paintItem(newItemObj);
+  shoppingItems.push(newItemObj);
+  saveShoppingItems();
   input.value = '';
   input.focus();
 }
